e47681f5c31ee5856669be033b79764cb94f7241
JKKross <zawadski.jkk@gmail.com>
2019-04-30 15:00:11 +0200

    Added more 'about the app' text

--- Red Zebra/View Controllers/AboutViewController.swift
+++ Red Zebra/View Controllers/AboutViewController.swift
@@ -16,12 +16,20 @@ class AboutViewController: CustomBaseViewController {
     @IBOutlet var settingsIconByLabel: UILabel!
     @IBOutlet var aboutTheAppLabel: UILabel!
     
+    let aboutTheAppText = #"""
+    Red Zebra is a simple text editor inspired by iOS app "Textor" & GNU's terminal text editor "NANO". Red Zebra is open-source software and you can find the source code here.
+
+    As stated previously, this app offers only basic text editing functionality in a familiar iOS Files-like user interface. If you're looking for something more "pro" & you are familiar with Vi-like editors, I'd highly recommend another open-source app - iVim.
+
+    Also, I highly appreciate any feedback and/or App Store review.
+    """#
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         versionNumberLabel.text    = "version 0.7"
         aboutTheAppLabel.textColor = .white
+        aboutTheAppLabel.text      = aboutTheAppText
     }
 
     

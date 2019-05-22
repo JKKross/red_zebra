fe1d3933e225618fdfec2072e1ef29d0cf200ec1
JKKross <zawadski.jkk@gmail.com>
2019-05-22 21:14:08 +0200

    Added ability for file names to begin with numbers or underscores

--- Red Zebra/View Controllers/DocumentBrowserViewController.swift
+++ Red Zebra/View Controllers/DocumentBrowserViewController.swift
@@ -68,7 +68,7 @@ class DocumentBrowserViewController: UIDocumentBrowserViewController, UIDocument
                 return
             }
             
-            guard fileName.first?.isLetter == true else {
+            guard (fileName.first?.isLetter == true || fileName.first?.isNumber == true || fileName.first == "_") else {
                 self.showErrorPopUp(message: #"You have to begin your file name with a letter ("a-z" or "A-Z")"#)
                 importHandler(nil, .none)
                 return

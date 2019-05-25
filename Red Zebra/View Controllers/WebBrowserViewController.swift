76dafcd2ce617e3a978082d5cd87f1bd2249e2a3
JKKross <zawadski.jkk@gmail.com>
2019-05-25 11:09:04 +0200

    Updated the preview-in-app browser

--- Red Zebra/View Controllers/WebBrowserViewController.swift
+++ Red Zebra/View Controllers/WebBrowserViewController.swift
@@ -9,25 +9,71 @@
 import UIKit
 import WebKit
 
-class WebBrowserViewController: CustomBaseViewController {
+class WebBrowserViewController: CustomBaseViewController, WKNavigationDelegate {
 
-    @IBOutlet var titleLabel: UINavigationItem!
     @IBOutlet var webView: WKWebView!
+    @IBOutlet var navBarTitleLabel: UINavigationItem!
+    @IBOutlet var backButtonLabel: UIBarButtonItem!
+    @IBOutlet var forwardButtonLabel: UIBarButtonItem!
     
     var webContent: WebContent?
     
     
     override func viewDidLoad() {
         
+        webView.navigationDelegate = self
+        
         let data = webContent?.data ?? ""
         webView.loadHTMLString(data, baseURL: webContent?.url)
+        
+        if let fileName = webContent?.url?.lastPathComponent {
+            navBarTitleLabel.title = fileName
+        } else {
+            navBarTitleLabel.title = ""
+        }
+        
+        backButtonLabel.isEnabled    = false
+        forwardButtonLabel.isEnabled = false
         
         super.viewDidLoad()
     }
     
     
-    @IBAction func dismissButton(_ sender: UIBarButtonItem) {
+    
+    @IBAction func closeButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true)
+    }
+    
+    
+    
+    @IBAction func backButton(_ sender: UIBarButtonItem) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    
+    @IBAction func forwardButton(_ sender: UIBarButtonItem) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    
+    
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        
+        if webView.canGoBack {
+            backButtonLabel.isEnabled = true
+        } else {
+            backButtonLabel.isEnabled = false
+        }
+        
+        if webView.canGoForward {
+            forwardButtonLabel.isEnabled = true
+        } else {
+            forwardButtonLabel.isEnabled = false
+        }
     }
     
     

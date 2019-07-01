ffc4080e3de1f7eadb84b12e8396943842b909c9
JKKross <zawadski.jkk@gmail.com>
2019-07-01 22:17:53 +0200

    Added unit tests (Again, this is part of a manual merge of now non-existing branch 'dev' into 'master')

---
+++ RedZebraTests/WordCountTests.swift
@@ -0,0 +1,59 @@
+//
+//  WordCountTests.swift
+//  RedZebraTests
+//
+//  Created by Jan Kříž on 1/07/2019.
+//  Copyright © 2019 Jan Kříž. All rights reserved.
+//
+
+import XCTest
+@testable import RedZebra
+
+class WordCountTests: XCTestCase {
+    
+    
+    let text = """
+    Hello there! 😎
+    We have a wonderful day/night here, right?
+
+    """
+    
+    let expectedChars = 58
+    let expectedBytes = 61
+    let expectedWords = 10
+    let expectedLines = 3
+    
+    let expectedItsTweetable = true
+    
+    var wc: WordCount!
+    
+    override func setUp() {
+        wc = WordCount(text: text)
+    }
+    
+    
+    func testCharacters() {
+        XCTAssert(expectedChars == wc.characters)
+    }
+    
+    
+    func testBytes() {
+        XCTAssert(expectedBytes == wc.bytes)
+    }
+    
+    
+    func testWords() {
+        XCTAssert(expectedWords == wc.words)
+    }
+    
+    
+    func testLines() {
+        XCTAssert(expectedLines == wc.lines)
+    }
+    
+    
+    func testItsTweetable() {
+        XCTAssert(expectedItsTweetable == wc.itsTweetable)
+    }
+    
+}

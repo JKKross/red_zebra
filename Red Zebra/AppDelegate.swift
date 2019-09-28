dff7e17dbebbfe43be010b8acb453f84891257f0
JKKross <zawadski.jkk@gmail.com>
2019-09-28 17:42:48 +0200

    Added basic support for Scenes.

--- Red Zebra/AppDelegate.swift
+++ Red Zebra/AppDelegate.swift
@@ -10,8 +10,6 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-    var window: UIWindow?
-    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
@@ -28,26 +26,32 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-    func application(_ app: UIApplication, open inputURL: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        // Ensure the URL is a file URL
-        guard inputURL.isFileURL else { return false }
-        
-        // Reveal / import the document at the URL
-        guard let documentBrowserViewController = window?.rootViewController as? DocumentBrowserViewController else { return false }
-        
-        documentBrowserViewController.revealDocument(at: inputURL, importIfNeeded: true) { (revealedDocumentURL, error) in
-            if let error = error {
-                // Handle the error appropriately
-                print("Failed to reveal the document at URL \(inputURL) with error: '\(error)'")
-                return
-            }
-            
-            // Present the Document View Controller for the revealed URL
-            documentBrowserViewController.presentDocument(at: revealedDocumentURL!)
-        }
-        
-        return true
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    
+//    func application(_ app: UIApplication, open inputURL: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+//        
+//        // Ensure the URL is a file URL
+//        guard inputURL.isFileURL else { return false }
+//        
+//        // Reveal / import the document at the URL
+//        guard let documentBrowserViewController = window?.rootViewController as? DocumentBrowserViewController else { return false }
+//        
+//        documentBrowserViewController.revealDocument(at: inputURL, importIfNeeded: true) { (revealedDocumentURL, error) in
+//            if let error = error {
+//                // Handle the error appropriately
+//                print("Failed to reveal the document at URL \(inputURL) with error: '\(error)'")
+//                return
+//            }
+//            
+//            // Present the Document View Controller for the revealed URL
+//            documentBrowserViewController.presentDocument(at: revealedDocumentURL!)
+//        }
+//        
+//        return true
+//    }
     
     
 }

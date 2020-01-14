a4bd3dea1ad5d18cc8cf50dcc7896bed545dab5e
JKKross <zawadski.jkk@gmail.com>
2020-01-14 12:18:14 +0100

    Added 2020 to copyright in About; moved to v2.1.1

--- Red Zebra/View Controllers/AboutViewController.swift
+++ Red Zebra/View Controllers/AboutViewController.swift
@@ -30,7 +30,7 @@ class AboutViewController: CustomBaseViewController {
     I don't have access to any of your personal data and/or information. The only way I can gain access to said information is if you decide to send me an e-mail. In that case, I will never willingly share your e-mail address or any private information included in that e-mail with anyone.
     
     
-    Red Zebra © 2019 Jan Kříž
+    Red Zebra © 2019-2020 Jan Kříž
     
     """#
     
